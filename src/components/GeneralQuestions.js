1434b5d916b2bee3c96ac1d752aa84ebca7cbbe9
vuurmot <97208800+vuurmot@users.noreply.github.com>
2024-05-01 08:08:59 +0800

    fix

--- src/components/GeneralQuestions.js
+++ src/components/GeneralQuestions.js
@@ -1,9 +1,12 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Button, Col, Form, Input, Row, Spin } from 'antd';
+import Markdown from 'react-markdown';
+import { createRoot } from 'react-dom/client';
+
 function GeneralQuestions() {
     const [searchInput, setSearchInput] = useState("");
-    const [data, setData] = useState({ message: 'Hi! How can I help you today?' });
+    const [data, setData] = useState(null);
     const [showSpin, setShowSpin] = useState(false);
     const [form] = Form.useForm();
 
@@ -25,18 +28,26 @@ function GeneralQuestions() {
 
     return (
         <Col span={20}>
-            <Row style={{ height: "90%" }}>
+            <Row style={{ height: "85%" }}>
                 {showSpin ?
                     <div style={{ margin: "auto" }}><Spin></Spin></div>
                     :
-                    <div style={{ margin: "auto", padding: 50, textAlign: 'left', maxWidth: 800 }}>
-                        <p>{searchInput && <h3>Question:</h3>} {searchInput}</p>
-                        <p>{searchInput && <h3>Answer:</h3>} {JSON.stringify(data.message)}</p>
-                    </div>
+                    data ?
+                        <div style={{ margin: "auto", padding: 50, textAlign: 'left', maxWidth: 800 }}>
+                            <h3>Question:</h3>
+                            <p> {searchInput}</p>
+                            <h3>Answer:</h3>
+                            <Markdown>{data.message}</Markdown>
+
+                        </div>
+                        :
+                        <div style={{ margin: "auto", padding: 50, textAlign: 'left', maxWidth: 800 }}>
+                            <p>Hi! How can I help you today?</p>
+                        </div>
 
                 }
             </Row>
-            <Row style={{ height: "10%", width: "100%", padding: 10 }}>
+            <Row style={{ height: "15%", width: "100%", padding: 10 }}>
                 <Form style={{ width: "100%" }} form={form} onFinish={retrieveData}>
                     <Form.Item style={{ marginBottom: 0 }} name="searchField"><Input ></Input></Form.Item>
                     <Form.Item style={{ marginBottom: 0 }}><Button type="primary" htmlType="submit">Submit</Button></Form.Item>
